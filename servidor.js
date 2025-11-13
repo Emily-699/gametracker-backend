d15671a1932ad3ff0fd424fe01393db0cb8da638
Emily <fabiana29vipar@gmail.com>
2025-11-12 20:09:57 -0500

    Cambio en el servidor

--- servidor.js
+++ servidor.js
@@ -59,7 +59,7 @@ app.use((err, req, res, next) => {
 });
 
 // Puerto y servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
